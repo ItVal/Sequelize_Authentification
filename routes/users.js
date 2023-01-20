696c8c4856538ef08064603001485df49f5951a9
ItVal <valentinhonvj1@gmail.com>
2023-01-20 09:29:16 +0100

    login user function and route

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,43 @@ router.post('/', async(req, res, next)=>{
   res.status(201).json(created_user);
 });
 
+//login user
+router.post('/login',async(req,res,next)=>{
+  const user = await User.findOne({ where : {email : req.body.email }});
+  if(user){
+     const password_valid = await bcrypt.compare(req.body.password,user.password);
+     if(password_valid){
+         token = jwt.sign({ "id" : user.id,"email" : user.email,"first_name":user.first_name },process.env.SECRET);
+         res.status(200).json({ token : token });
+     } else {
+       res.status(400).json({ error : "Password Incorrect" });
+     }
+   
+   }else{
+     res.status(404).json({ error : "User does not exist" });
+   }
+   
+   });
 
+//Securing a route using middleware and JWT verification
+router.get('/me',
+ async(req,res,next)=>{
+  try {
+    let token = req.headers['authorization'].split(" ")[1];
+    let decoded = jwt.verify(token,process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch(err){
+    res.status(401).json({"msg":"Couldnt Authenticate"});
+  }
+  },
+  async(req,res,next)=>{
+    let user = await User.findOne({where:{id : req.user.id},attributes:{exclude:["password"]}});
+    if(user === null){
+      res.status(404).json({'msg':"User not found"});
+    }
+    res.status(200).json(user);
+ }); 
 
 
 module.exports = router;
